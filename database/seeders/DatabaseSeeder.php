b052ff58a91b1e1b772879a7a1d65bab0d6b35a2
joneboop <joonastj.korkiakoski@gmail.com>
2026-03-14 13:29:06 -0600

    commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,10 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Database\Seeders\MembershipSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,18 +12,10 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-        public function run(): void
-        {
-            // User::factory(10)->create();
-
-            User::factory()->create([
-                'name' => 'Test User',
-                'email' => 'test@example.com',
-            ]);
-
-            $this->call([
-                MembershipSeeder::class,
-            ]);
-        }
-    
+    public function run(): void
+    {
+        $this->call([
+            MembershipSeeder::class,
+        ]);
+    }
 }
